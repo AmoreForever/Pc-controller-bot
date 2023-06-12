f5e808895815414ec657daf1b2eeb7edfb579b0d
Amore <103936585+AmoreForever@users.noreply.github.com>
2023-06-12 23:49:42 +0500

    Update version.py

--- misc/version.py
+++ misc/version.py
@@ -4,14 +4,13 @@ import time
 CACHE_TIMEOUT = 60   # Cache timeout in seconds
 cache_expires_at = 0  # When the cache expires
 cached_sha = None     # Cached sha value
-headers = {"Authorization": f"Token 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"}
 def get_latest_commit_sha():
     global cache_expires_at, cached_sha
     
     now = time.time()
     if now >= cache_expires_at:
         url = "https://api.github.com/repos/AmoreForever/pc-controller-bot/commits"
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         if response.status_code == 200:
             if commits := response.json():
                 latest_commit = commits[0]
